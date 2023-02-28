69f2682f305194df665cb6ab666274002ad1f082
Leland Batey <lelandbatey@lelandbatey.com>
2023-02-28 13:22:55 -0800

    Remove unused code moved to timeformat

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -15,7 +14,6 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
-	"text/template"
 	"time"
 
 	timefmt "github.com/itchyny/timefmt-go"
@@ -210,13 +208,13 @@ func read_lines_to_integers(r io.Reader, parsefunc func(string) (time.Time, erro
 		if parsefunc == nil {
 			ts, err = strconv.ParseInt(line, 10, 64)
 			if err != nil {
-				fmt.Fprint(os.Stderr, GuessTimestampFormat(line))
+				fmt.Fprint(os.Stderr, timeformat.GuessTimestampFormat(line))
 				return nil, fmt.Errorf("cannot parse integer on line %d of stdin: %w\n", i, err)
 			}
 		} else {
 			t, err := parsefunc(line)
 			if err != nil {
-				fmt.Fprint(os.Stderr, GuessTimestampFormat(line))
+				fmt.Fprint(os.Stderr, timeformat.GuessTimestampFormat(line))
 				return nil, fmt.Errorf("cannot parse line %d of stdin to date: %w", i, err)
 			}
 			ts = t.UnixMilli()
@@ -243,100 +241,4 @@ func openbrowser(url string) {
 		log.Printf("cannot launch web browser: %v\n", err)
 	}
 
-}
-
-func GuessStrptimeFormat(line string) (string, string, error) {
-	type tsfmt struct {
-		Name string
-		Fmt  string
-	}
-	potenials := []tsfmt{
-		{"RFC3339", "%Y-%m-%dT%H:%M:%S.%f%z"},
-		{"RFC3339", "%Y-%m-%dT%H:%M:%S%z"},
-		{"YYYY-mm-dd HH:MM:SS.ms", "%Y-%m-%d %H:%M:%S.%f"},
-		{"YYYY-mm-dd HH:MM:SS.ms TZ", "%Y-%m-%d %H:%M:%S.%f %z"},
-		{"YYYY-mm-dd HH:MM:SS", "%Y-%m-%d %H:%M:%S"},
-		{"YYYY-mm-dd HH:MM:SS TZ", "%Y-%m-%d %H:%M:%S %z"},
-		{"YYYY-mm-dd", "%Y-%m-%d"},
-	}
-	for _, candidate := range potenials {
-		_, err := timefmt.Parse(line, candidate.Fmt)
-		if err == nil {
-			return candidate.Name, candidate.Fmt, nil
-		}
-	}
-	return "", "", fmt.Errorf("could not parse %q with any known strptime formats", line)
-}
-
-func GuessGoTimeFormat(line string) (string, string, error) {
-	type tsfmt struct {
-		Name string
-		Fmt  string
-	}
-	potenials := []tsfmt{
-		// Note that we do not need a timestamp specifier for RFC3339 with
-		// micro/mili/nano seconds, just one with whole seconds. This is
-		// because of a documented quirk of how the official Go time library
-		// parses formats; to quote the documentation:
-		//
-		// > When parsing (only), the input may contain a fractional second field
-		// > immediately after the seconds field, even if the layout does not
-		// > signify its presence. In that case either a comma or a decimal point
-		// > followed by a maximal series of digits is parsed as a fractional
-		// > second.
-		//
-		// So basically, even if you don't include fractional seconds in the
-		// timestamp specifier, Go will still parse the fractional second.
-		{"RFC3339", "2006-01-02T15:04:05.999Z07:00"},
-		{"RFC1123", time.RFC1123},
-		{"RFC1123Z", time.RFC1123Z},
-		{"RFC822", time.RFC822},
-		{"RFC822Z", time.RFC822Z},
-		{"YYYY-mm-dd HH:MM:SS.ms", "2006-01-02 15:04:05.999"},
-		{"YYYY-mm-dd HH:MM:SS.ms TZ", "2006-01-02 15:04:05.999 -07:00"},
-		{"YYYY-mm-dd", "2006-01-02"},
-	}
-	for _, candidate := range potenials {
-		_, err := time.Parse(candidate.Fmt, line)
-		if err == nil {
-			return candidate.Name, candidate.Fmt, nil
-		}
-	}
-	return "", "", fmt.Errorf("could not parse %q with any known gotime formats", line)
-}
-
-func GuessTimestampFormat(line string) string {
-	hinttempl := `
-HINT: It looks like the timestamp '{{.UnknownLine}}' is in a format named
-'{{.FormatName}}' with a format specification of '{{.FormatContents}}'. To parse
-all the incoming timestamps as format '{{.FormatName}}', provide the following option:
-
-	{{.FlagName}} '{{.FormatContents}}'
-
-`
-
-	var tmpl = template.Must(template.New("FormatHintTemplate").Parse(hinttempl))
-	type guessfmt struct {
-		FlagName string
-		FmtFunc  func(string) (string, string, error)
-	}
-	guessers := []guessfmt{
-		{"--gotime-fmt", GuessGoTimeFormat},
-		{"--strptime-fmt", GuessStrptimeFormat},
-	}
-
-	for _, g := range guessers {
-		fmtname, format, err := g.FmtFunc(line)
-		if err == nil {
-			buf := &bytes.Buffer{}
-			tmpl.Execute(buf, map[string]string{
-				"UnknownLine":    line,
-				"FormatName":     fmtname,
-				"FormatContents": format,
-				"FlagName":       g.FlagName,
-			})
-			return buf.String()
-		}
-	}
-	return "HINT: Use the '--strptime-format' flag to indicate the format of the incoming timestamps\n\n"
 }
